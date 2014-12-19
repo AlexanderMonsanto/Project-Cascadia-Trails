68a6344bcb7c6f141e5818aa76502578e5228109
alexandermonsanto <alexandermonsanto@gmail.com>
2014-12-19 02:15:01 -0800

    bug fix 5

--- public/jquery.js
+++ public/jquery.js
@@ -37,6 +37,7 @@ $(function(){
 
             $(function() {
                 $(".paragraphs").hover(function(){
+
                     $(this).show();
                 }, function () {
                     $(this).hide();
@@ -44,7 +45,38 @@ $(function(){
             });
 
 });
+$(function(){
 
+            var x = 0;
+            var y = 0;
+            var imageHeight=1100;
+            var imageWidth=3000;
+            var direction = -1;
+
+            //cache a reference to the background
+            var background = $('#container-fluid');
+
+            // set initial background background position
+            background.css('backgroundPosition', x + 'px' + ' ' + y + 'px');
+
+            // scroll up background position every 90 milliseconds
+
+            window.setInterval(function() {
+                background.css("backgroundPosition", x + 'px' + ' ' + y + 'px');
+                y=y+direction;
+                if(direction < 0 && y+imageHeight < $(document).height()-10){
+                    direction=1;
+                } else if(direction > 0 && y > -10){
+                    direction=-1
+                };
+
+                //if you need to scroll image horizontally -
+                // uncomment x and comment y
+
+            }, 120);
+
+
+});
 
 
 
